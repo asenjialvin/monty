d8f3eb069a4c6f19065735c496b28b63bb2c4f2d
asenjialvin <asenjialvin@gmail.com>
2023-12-18 11:06:28 -0800

    Added task 0

--- monty.h
+++ monty.h
@@ -52,7 +52,8 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
+/*ssize_t getstdin(char **lineptr, int file);*/
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 char  *clean_line(char *content);
 void f_push(stack_t **head, unsigned int number);
 void f_pall(stack_t **head, unsigned int number);
